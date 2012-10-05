4650777fc8c10a901db37c4fdb55751c0432e63f
Paul Morie <pmorie@gmail.com>
2012-10-05 14:04:36 -0400

    Add html_parse getter to @app

--- .openshift/tests/step_definitions/cakephp-example_steps.rb
+++ .openshift/tests/step_definitions/cakephp-example_steps.rb
@@ -3,7 +3,8 @@ require 'nokogiri'
 
 And /^I load the application$/ do 
   parse = Nokogiri::HTML("http://#{@app.name}-#{@account.domain}.dev.rhcloud.com")
-  @app.instance_variable_set("@html_parse", parse)
+  @app.instance_variable_set(:@html_parse, parse)
+  @app.instance_eval('def html_parse; @html_parse; end')
 end
 
 And /^the quickstart will report readiness$/ do
